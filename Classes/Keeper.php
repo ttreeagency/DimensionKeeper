e458a666bc777bb16bf1051ec34f01afc0a89bba
Dominique Feyer <dfeyer@ttree.ch>
2017-10-05 00:21:33 +0200

    TASK: Remove property tracker

--- Classes/Keeper.php
+++ Classes/Keeper.php
@@ -19,11 +19,6 @@ class Keeper
     protected $systemLogger;
 
     /**
-     * @var array
-     */
-    protected $tracker = [];
-
-    /**
      * @var bool
      * @Flow\InjectConfiguration(path="enabled")
      */
@@ -35,19 +30,10 @@ class Keeper
             return;
         }
 
-        $originalContextPath = $node->getContextPath();
-        $key = md5($originalContextPath . $propertyName . \serialize($newValue));
-        if (isset($this->tracker[$key]) && $this->tracker[$key] === true) {
-            $this->systemLogger->log(\vsprintf('Skip synchronization property %s from node %s', [$propertyName, $originalContextPath]), \LOG_DEBUG, null, 'Ttree.DimensionKeeper');
-            return;
-        }
-
         \array_map(function (NodeInterface $nodeVariant) use ($propertyName, $newValue) {
             $this->systemLogger->log(\vsprintf('Synchronize property %s to node variant %s', [$propertyName, $nodeVariant->getContextPath()]), \LOG_DEBUG, null, 'Ttree.DimensionKeeper');
             $nodeVariant->setProperty($propertyName, $newValue);
         }, $node->getOtherNodeVariants());
-
-        $this->tracker[$key] = true;
     }
 
     public function skip(\Closure $closure)
